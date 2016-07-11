3012f8b98b46eea504b2b769b657c16216987af2
Ronda Bergman <rondabergman@hotmail.com>
2016-07-11 18:47:27 -0400

    Initital commit

---
+++ Movies/Movies/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        MovieDBEntities context = new Movies.MovieDBEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View(context.Movies.ToList());
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
