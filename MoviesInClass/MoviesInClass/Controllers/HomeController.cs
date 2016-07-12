a0def68087f81c54a88d2cfb137199730eba17a9
Ronda Bergman <rondabergman@hotmail.com>
2016-07-12 15:19:05 -0400

    Initial Commit

---
+++ MoviesInClass/MoviesInClass/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesInClass.Controllers
+{
+    public class HomeController : Controller
+    {
+        MovieDBEntities db = new MoviesInClass.MovieDBEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Now doing Inventory";
+
+            Inventory inv = db.Inventories.Find(4);
+
+            return View(inv);
+        }
+
+        public ActionResult Contact()
+        {
+            var rentals = from r in db.Rentals
+                          where r.CustomerID == 1 && r.DiskID == 2
+                          select r;
+
+            Rental rental = null;
+            if (rentals.Count() > 0)
+                rental = rentals.First();
+
+            return View(rental);
+        }
+    }
+}
---
+++ MoviesInClass/MoviesInClass/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoviesInClass.MvcApplication" Language="C#" %>
